8e43718d2859c0adeefd7824a68dac6413e8921e
JoeKabongo <jonathankabongo23@gmail.com>
2021-01-05 19:01:15 -0800

    setup db connection

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import taskRoutes from './routes/tasks.js';
+import CONNECTION_URL from './db.js';
+
+const app = express();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error));
+
+mongoose.set('useFindAndModify', false);
+
+// routes
+app.use('/tasks', taskRoutes);
