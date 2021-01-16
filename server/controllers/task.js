d19d9411e3e6b5e1b98b3f96680e5b08ced19ae3
JoeKabongo <jonathankabongo23@gmail.com>
2021-01-15 22:02:38 -0800

    delete task

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import Task from '../models/task.js';
 
+// Get all tasks for a user
 export async function getTasks(req, res) {
   try {
     const tasks = await Task.find({ userId: req.user.userId });
@@ -10,6 +11,7 @@ export async function getTasks(req, res) {
   }
 }
 
+// create a task for a user
 export async function createTask(req, res) {
   const { name } = req.body;
 
@@ -26,27 +28,27 @@ export async function createTask(req, res) {
   }
 }
 
+// delete user task
 export async function deleteTask(req, res) {
   const { id } = req.params;
   try {
     await Task.findByIdAndDelete(id);
-    res.status(200).json({ message: 'Post was deleted' });
+    res.status(200).json({ message: 'Task was deleted' });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ error: 'Something went wrong' });
   }
 }
 
+// update a user task
 export async function updateTask(req, res) {
-  const { id } = req.params;
-  const { name, isCompleted } = req.body;
-  console.log('HERRE');
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
-
-  const updatedTask = { name, isCompleted };
-
-  const newTask = await Task.findByIdAndUpdate(id, updateTask, { new: true });
-
-  res.json(newTask);
+  // const { id } = req.params;
+  // const { name, isCompleted } = req.body;
+  // if (!mongoose.Types.ObjectId.isValid(id))
+  //   return res.status(404).send(`No task with id: ${id}`);
+  // try {
+  //   const newTask = await Task.findByIdAndUpdate(id, { name, isCompleted });
+  //   res.json(newTask);
+  // } catch (error) {
+  //   res.status(500).json({ error: 'Something went wrong' });
+  // }
 }
